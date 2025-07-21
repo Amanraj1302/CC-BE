91722c2242c1c57c1c87cbe9d6a97837c2b1b7cc
Amanraj1302 <amanraaj239@gmail.com>
2025-07-21 23:37:34 +0530

    port code update

--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,12 @@ app.use("/api/users", userRoutes);
 app.use("/api/artist", artistRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/api/project",ProjectFormRoutes)
-
+const Port = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI!)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => {
-      console.log("Server running on port" + "http://localhost:5000");
+    app.listen(Port, () => {
+      console.log("Server running on port" + `http://localhost:${Port}`);
     });
   })
   .catch(err => console.error(err));
